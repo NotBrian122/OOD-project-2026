7ff27f0ca1b506647df625f425c7596a4817df6a
NotBrian122 <s00254527@atu.ie>
2026-02-02 17:35:24 +0000

    Creating the base card class

---
+++ Cards.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_project_2026
+{
+    internal class Cards
+    {
+        public string CardName { get; set; }
+        public int  CardChipValue { get; set; }
+        public bool FaceCard { get; set; }
+
+        public Cards(string cardName, int cardChipValue, bool faceCard)
+        {
+            CardName = cardName;
+            CardChipValue = cardChipValue;
+            FaceCard = faceCard;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{CardName} - Value: {CardChipValue}, Face Card: {FaceCard}";
+        }
+
+    }
+}
