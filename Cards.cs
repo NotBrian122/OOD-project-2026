941c7ec8e43908d09d6ca3d2b1cfd1410bb72a0a
NotBrian122 <s00254527@atu.ie>
2026-02-09 22:02:21 +0000

    changes made, starting classes and other shite

--- Cards.cs
+++ Cards.cs
@@ -6,24 +6,55 @@ using System.Threading.Tasks;
 
 namespace OOD_project_2026
 {
-    internal class Cards
+    internal class Cards :Deck
     {
+        //obvs name
         public string CardName { get; set; }
+        //each card has a "chip value"
         public int  CardChipValue { get; set; }
+        //Face cards have special effects for certian joker cards
         public bool FaceCard { get; set; }
+        //SuitName is obvs 
+        public  string SuitName { get; set; }
+        //Cards can have a modifier so I have to leave this speace clear.
+        public string Effect {  get; set; }
 
-        public Cards(string cardName, int cardChipValue, bool faceCard)
+        public Cards() { }
+        public Cards(string CardName, int CardChipValue, string SuitName, string Effect)
         {
-            CardName = cardName;
-            CardChipValue = cardChipValue;
-            FaceCard = faceCard;
+
+            this.SuitName = SuitName;
+            Effect = Effect;
+
+            switch (CardChipValue)
+            {
+                case 1:
+                    this.CardName = "Ace";
+                    break;
+                case 11:
+                   this. CardName = "Jester";
+                    FaceCard = true;
+                    break;
+                case 12:
+                    this.CardName = "Queen";
+                    FaceCard = true;
+                    break;
+                case 13:
+                    this.CardName = "King";
+                    FaceCard = true;
+                    break;
+                case 14:
+                    this.CardName = "Ace";
+                    break;
+                default:
+                    this.CardName = CardChipValue.ToString();
+                    break;
+            }
         }
-
-
-        public override string ToString()
-        {
-            return $"{CardName} - Value: {CardChipValue}, Face Card: {FaceCard}";
-        }
-
+        
     }
+        
 }
+
+
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,11 +18,55 @@ namespace OOD_project_2026
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        List<Cards> PlayerDeck = new List<Cards>();
         public MainWindow()
         {
             InitializeComponent();
+            //creating new list to create deck to add or subtrack cards from.
+            
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateDeck();
+            //calling the generating deck metho as to easily create something on click 
+        }
+        public void GenerateDeck()
+        {
+            //creating a for loop to generate the deck of all cards needed
+            for (int i = 0; i < 4; i++)//every suit for every deck
+            {
+                //counts each card number
+                for (int j = 1; j < 14; j++)
+                {
+                    switch (i)
+                    {
+                        //public Cards(string CardName, int CardChipValue, string SuitName, string Effect)
+                        case 0:
+                            Cards cardH = new Cards($"{j}",j,"Hearts", "None");//so a suit then a value of 1 => 14
+                            PlayerDeck.Add(cardH);
+
+                            break;
+                        case 1:
+                            Cards cardD = new Cards($"{j}",j, "Diamonds","None");
+                           PlayerDeck.Add(cardD);
+                            break;
+                        case 2:
+                            Cards cardS = new Cards($"{j}",j, "Spades","None");
+                           PlayerDeck.Add(cardS);
+                            break;
+                        case 3:
+                            Cards cardC = new Cards($"{j}",j, "Clubs","None");
+                            PlayerDeck.Add(cardC);
+                            break;
+                    }
+                }
+                
+            }
+
         }
     }
 }
