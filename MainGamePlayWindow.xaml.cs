c2817b701e6ad1c5697e7896fc26c3e0d054b330
NotBrian122 <158192983+NotBrian122@users.noreply.github.com>
2026-03-18 23:23:07 +0000

    stuff isint working

--- MainGamePlayWindow.xaml.cs
+++ MainGamePlayWindow.xaml.cs
@@ -22,18 +22,55 @@ namespace OOD_project_2026
 
     //want to load deck and do card gen outside of the main page so everything can work together
 
-
     public partial class MainGamePlayWindow : Page
     {
+        //Declaring the hands that are there /left
+
+         int handsLeft = 3;
+         int disguardsLeft = 3;
+         int selectedCards = 0;
+
+        //genereating a new deck
+        Deck deck = new Deck();
+        //generating hands 
+        List<Cards> PotentionalHand = new List<Cards>();
+        //records the hands played
+        List<Cards> HandPlayed = new List<Cards>();
+        //records the hands disguarded
+        List<Cards> HandDiscarded = new List<Cards>();
+        //generates a list of joker cards with spesific modifiers and effects.
+        List<JokerCards> Jokers = JokerCards.GenerateJokerCards();
+
+        //generates blank player class - this should be initialzed once 
+        Player Player = new Player();
+        //creating the deck - all good here. 
+        
+
         public MainGamePlayWindow()
         {
             InitializeComponent();
+
         }
 
         private void CardGrid_Loaded(object sender, RoutedEventArgs e)
         {
             //so this is the section in which the random cards are given from the deck.
             //The grid is loaded. 
+
+
+            Random random = new Random();
+            int randomNUmber =random.Next(0,deck.FullDeck.Count);
+            //this is the hand list that the player will get. Completely random.
+            List<Cards> hand = new List<Cards>();
+
+            //this is for adding random numbers to your hand
+            for (int i = 0; i < 10; i++)
+            {
+                randomNUmber = random.Next(0, deck.FullDeck.Count);
+                hand.Add(deck.FullDeck[randomNUmber]);
+                //this is to remove from the deck so we dont get duplicates in our hand
+                deck.FullDeck.RemoveAt(randomNUmber);
+            }
 
 
         }
@@ -44,7 +81,7 @@ namespace OOD_project_2026
             
 
             //load deck 
-
+            deck.CreateDeck();
            
         }
     }
