752e3506f093ebf54bce583f4a8b71667a01c608
NotBrian122 <158192983+NotBrian122@users.noreply.github.com>
2026-03-31 00:45:57 +0100

    Changed the doe to make it easier to read for myself

--- MainGamePlayWindow.xaml.cs
+++ MainGamePlayWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Automation.Peers;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OOD_project_2026
 {
@@ -42,10 +44,11 @@ namespace OOD_project_2026
         List<Cards> selectedHand = new List<Cards>();
         List<Cards> HandPlayed = new List<Cards>();
         List<Cards> HandDiscarded = new List<Cards>();
-       
+
         List<JokerCards> JokerCardsInPlay = new List<JokerCards>();
         Player Player = new Player();//I want to add a player class to write to a file. This will track your best score.
         Random random = new Random();
+
 
         #endregion
         public MainGamePlayWindow()
@@ -58,25 +61,25 @@ namespace OOD_project_2026
             //creating the deck. it works. 
             deck.CreateDeck();
             //Generating a list of jokers.
-             List<JokerCards> Jokers = JokerCards.GenerateJokerCards();
+            List<JokerCards> Jokers = JokerCards.GenerateJokerCards();
             //drawing the cards 
             DrawCards(8);
             //refreshing the ui before we start the game. 
             RefreshHandUI();
             //Generating blind score 
             round++;
-            BlindScoreDisplay.Text = $"{GenerateBlindScore(round)}";
+            BlindScoreDisplay.Text = $"BlindScore: {GenerateBlindScore(round)}";
         }
-       
+
         #region Updating ui/ clard clicking 
         //I created a method that Refreshes the card slots once a hand was played.
         private void RefreshHandUI()
         {
             //this is the list of cards that are in the grid. refresing the list when called. 
-         List<Button> cardSlots = new List<Button>()
-        {HandCard1,HandCard2,HandCard3,HandCard4,HandCard5,HandCard6,HandCard7,HandCard8};
+            List<Button> cardSlots = new List<Button>()
+            {HandCard1,HandCard2,HandCard3,HandCard4,HandCard5,HandCard6,HandCard7,HandCard8};
 
-            //this is to add the content to the displayed hand.
+
             for (int i = 0; i < cardSlots.Count; i++)
             {
                 if (i < hand.Count)//created a hand that gives the player 10 cards. 
@@ -102,7 +105,7 @@ namespace OOD_project_2026
             HandsLeft.Text = $"Hands Left:{handsLeft}";
             DisguardsLeft.Text = $"Disguards Left:{disguardsLeft}";
             //after refreshing the hand ui I do a check for if the player has won or not. 
-            CheckWin(playersCurrentScore,handsLeft,disguardsLeft,blindScore);
+            CheckWin(playersCurrentScore, handsLeft, disguardsLeft, blindScore);
         }
 
         //playing cards method. 
@@ -155,76 +158,98 @@ namespace OOD_project_2026
             {
                 //this refreshes the children of the grid.
                 btn.Background = Brushes.White;
-                btn.Margin = new Thickness(0,0,0,0);
+                btn.Margin = new Thickness(0, 0, 0, 0);
             }
 
             selectedHand.Clear();
             RefreshHandUI();
 
             disguardsLeft--;
-        
+
         }
         private void PlayHand_Click(object sender, RoutedEventArgs e)
         {
-            //just in case you try and waste a hand. 
+            //just in case you try and waste a hand. You have to play something to advance the game.
             if (selectedHand.Count == 0)
+            {
                 return;
+            }
+                
             //this is for the score, they are doubles as the alrger scores and some other cards
             //can fuck with intagers so Ive started with this. 
-            double chipScore = 0;
-            double multScore = 0;
+            double chipScore = 0, multScore = 0;
             int handNumber = 0;
             string handPlayed = "";//type of hand played
             //checking for staights. 
             bool isFlush = false;
             int isPair = 0;
 
-           //Wanted to sort the hand before hand as it would make it easier to score. 
-            selectedHand.Sort();
+            //Assinging the card to the CardsPlayedSection 
+            List<TextBlock> cardSlotsPlayed = new List<TextBlock>()
+            { CardPlayed1,CardPlayed2,CardPlayed3,CardPlayed3,CardPlayed4,CardPlayed5};
 
-            //this for loop is to compare if the selected hands suits a the same. 
+            //adding the 2 string to the cards.  
             for (int i = 0; i < selectedHand.Count; i++)
             {
-                //this compares the previous card to the current one for the chip value. 
-                //afterwards I have to check for straights
-                //this is for pairs
-                if (i != 0 && selectedHand[i].CompareTo(selectedHand[i - 1]) == 0)
-                {
-                    isPair++;//this counts the amount of pairs in your hand. This can include 2 pair. 
-                    handNumber++;
-                    
-                    //this is for edgecases for straights making ace high or low depnding 
-                } else if (CheckStriaght(selectedHand))
-                {
-                    handNumber = 4;//I have to check for aces as 2 is below it it counts as a straight
-                    break;
+                cardSlotsPlayed[i].Text = selectedHand[i].ToString();//this gives an illusion of cards played. 
 
-
-                }
-                //adding the chip score from each of the hands to this. 
-                chipScore += selectedHand[i].CardChipValue;
             }
-            #region check for straights
-            //checking if the hand count is 5 for straights. 
-            if (selectedHand.Count == 5)
+            //Wanted to sort the hand before hand as it would make it easier to score. 
+            selectedHand.Sort();
+
+            //im going to change these sections here 
+
+            //pair
+            if (CheckPair(selectedHand, isPair) == 1)
             {
-                //setting this true before checking as it makes it easier. 
-                isFlush = true;
+                handNumber = 1;//this is for a pair 
+            }
+            // 2 pair 
+            else if (CheckPair(selectedHand,isPair) == 2)
+            {
+                handNumber = 3;//trying to get 2 pair
+            //3 of a kind
+            }else if (CheckThreeOfAKind(selectedHand)){
 
-                for (int i = 1; i < selectedHand.Count; i++)
+                handNumber = 2;//this is for 3 of a kind.
+            }
+            //stright     
+            else if (CheckStriaght(selectedHand))
+            {
+                handNumber = 4;//I have to check for aces as 2 is below it previously counts as a straight
+            }
+            //flush.  
+
+            //Then adding the chip score. This is where the animation is going to take palce. 
+                
+
+               
+
+
+                //this for loop is to compare if the selected hands suits a the same. 
+                for (int i = 0; i < selectedHand.Count; i++)
                 {
-                    //comparing suit names and values to see if you get a flsuh or not 
-                    if (i!=0 && selectedHand[i].SuitName != selectedHand[i-1].SuitName)
+                    //this compares the previous card to the current one for the chip value. 
+                    //afterwards I have to check for straights
+                    //this is for pairs
+                    if (i != 0 && selectedHand[i].CompareTo(selectedHand[i - 1]) == 0)
                     {
-                        isFlush = false;
-                        break;
+                        isPair++;//this counts the amount of pairs in your hand. This can include 2 pair. 
+
+                        //this is for edgecases for straights making ace high or low depnding 
                     }
+                    
+                    //adding the chip score from each of the hands to this. 
+                    chipScore += selectedHand[i].CardChipValue;
                 }
+            #region check for flushes
+            //checking if the hand count is 5 for flushes. 
+            if (selectedHand.Count == 5)
+            {   
+                isFlush = true;
+                CheckFlush(isFlush, selectedHand);
             }
-            //trying to get 2 pair
-            if(isPair == 2){
-                handNumber = 3;
-            }
+          
             //this makes straights so much easier to keep a track of. 
             if (isFlush)
             {
@@ -299,7 +324,6 @@ namespace OOD_project_2026
             PlayerChipScore.Text = finalScore;
             CheckWin(playersCurrentScore, handsLeft, disguardsLeft, blindScore);
         }
-
         #endregion 
         private void DrawCards(int amount)
         {
@@ -310,17 +334,17 @@ namespace OOD_project_2026
                 if (deck.FullDeck.Count == 0)
                     return;
                 //checking for a new random number inside of the deck
-                int newCards = random.Next(0,deck.FullDeck.Count);
+                int newCards = random.Next(0, deck.FullDeck.Count);
                 //adding the drawn cards to the deck. 
                 Cards drawnCard = deck.FullDeck[newCards];
                 hand.Add(drawnCard);
                 deck.FullDeck.RemoveAt(newCards);
             }
         }
-        private void CheckWin(double PlayerChipScore,int handsLeft, int disguardsLeft, double BlindScore)
+        private void CheckWin(double PlayerChipScore, int handsLeft, int disguardsLeft, double BlindScore)
         {
             double comparingScore = BlindScore - PlayerChipScore;
-            if( comparingScore <= 0 && handsLeft != 0)
+            if (comparingScore <= 0 && handsLeft != 0)
             {
                 //you win
                 //win window; this will allow you to go to the shop. 
@@ -336,33 +360,33 @@ namespace OOD_project_2026
             else
             {
                 //continue playing aka nothing happens. 
-            }
+            } 
+            //I need to reset blind score after the player is defeated or advance it if they win. 
         }
-        //I need to generate a blind score for the player to "Defeat"
-        private int GenerateBlindScore(int roundScore) 
+        private int GenerateBlindScore(int roundScore)
         {
-           if(roundScore > 1)
+            if (roundScore > 1)
             {
-                blindScore += blindScore * roundScore ;
+                blindScore += blindScore * roundScore;
                 return blindScore;
             }
-            else 
+            else
                 return blindScore;
         }
-        private bool CheckStriaght(List<Cards>selectedHand)
+        private bool CheckStriaght(List<Cards> selectedHand)
         {
             bool isStraight = false;
-            if(selectedHand.Count == 5)//checking if the hand has 5 cards.
+            if (selectedHand.Count == 5)//checking if the hand has 5 cards.
             {
                 for (int i = 0; i < selectedHand.Count; i++)
                 {
                     //this is for a regular striaght, as your taking away the previous card chip value from the current one 
-                    if ((i!=0 && selectedHand[i].CardChipValue - selectedHand[i-1].CardChipValue == 1)
+                    if ((i != 0 && selectedHand[i].CardChipValue - selectedHand[i - 1].CardChipValue == 1)
                         // this is to check for a singualr hand as this edgecase is tough to do with a loop 
-                        || (selectedHand[0].CardChipValue == 2 
+                        || (selectedHand[0].CardChipValue == 2
                         && selectedHand[1].CardChipValue == 3
                         && selectedHand[2].CardChipValue == 4
-                        && selectedHand[3].CardChipValue ==5 
+                        && selectedHand[3].CardChipValue == 5
                         && selectedHand[4].CardChipValue == 14))//its a low straight but the chipvalue of the ace is 14 but it also counts as a 1 
                     {
                         isStraight = true;
@@ -370,8 +394,54 @@ namespace OOD_project_2026
                     }
                 }
                 return isStraight;
-            }else 
-            return isStraight;
+            }
+            else
+                return isStraight;
+        }
+        private bool CheckFlush(bool isFlush, List<Cards> selectedHand)
+        {
+            if (isFlush)
+            {
+                for (int i = 1; i < selectedHand.Count; i++)
+                {
+                    //comparing suit names and values to see if you get a flsuh or not 
+                    if (i != 0 && selectedHand[i].SuitName != selectedHand[i - 1].SuitName)
+                    {
+                        isFlush = false;
+                       
+                    }
+                }
+            }
+            return isFlush;
+        }
+        private int CheckPair(List<Cards> selectedHand, int isPair)
+        {
+            for (int i = 0; i < selectedHand.Count; i++)
+            {
+                if (i != 0 && selectedHand[i].CompareTo(selectedHand[i - 1]) == 0)
+                {
+                    isPair++;//this counts the amount of pairs in your hand. This can include 2 pair. 
+                }
+            }
+            return isPair;
+        }
+        private bool CheckThreeOfAKind(List<Cards> selectedHand)
+        {
+            int isThreeOfAKind = 0;
+            for (int i = 0; i < selectedHand.Count; i++)
+            {
+                if (i != 0 && selectedHand[i].CompareTo(selectedHand[i - 1]) == 0 )
+                {
+                    isThreeOfAKind++;//this counts the amount of pairs 
+                }
+            }
+            
+            if(isThreeOfAKind == 3)
+            {
+               return true;
+            }
+            else
+                return false;
         }
     }
 }
