4b4fb9ce71d7b885fe457a54389dd64373a13704
NotBrian122 <158192983+NotBrian122@users.noreply.github.com>
2026-04-11 15:47:03 +0100

    started arcana cards have to go to work so this is all ive got done today

--- ArcanaCards.cs
+++ ArcanaCards.cs
@@ -8,5 +8,21 @@ namespace OOD_project_2026
 {
     internal class ArcanaCards
     {
+        public string CardName { get; set; }
+        public string EffectDiscription { get; set; }
+        public int NoCardsAffected { get; set; }    
+
+        public string CardAffects { get; set; }
+        public List<ArcanaCards> ArcarnaCardsList { get; set; }
+        public ArcanaCards() { }
+
+        public List<ArcanaCards> GenreateCarcanraCards()
+        {
+            string[] CardName = { };
+            string[] cardDiscription = { "Select 2 cards to give them this affect \n With this affect is given 4 mult" };
+            return ArcarnaCardsList;
+        }
+
+
     }
 }
